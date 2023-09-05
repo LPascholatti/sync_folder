a7febd428cbcc4cbd901195070d2e91f0db874bb
lpascholatti-dexter <lucas@dexterenergy.nl>
2023-09-05 13:56:44 +0200

    Commit print files

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
-#Source path here:
+# Source path here:
 source_dir = "./source"
 
-#Assert that source is a directory:
+# Assert that source is a directory:
 assert os.path.isdir(source_dir), f"{source_dir} is not a directory"
 
 files_source = os.listdir(source_dir)
 
+print(f"These are the files at {source_dir}:")
 for file in files_source:
     print(file)
