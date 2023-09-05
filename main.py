b76f1313a193bc8e0d01f0a7b92a5220b59b4ae0
lpascholatti-dexter <lucas@dexterenergy.nl>
2023-09-05 14:23:28 +0200

    Copy files from source to replica folder

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import os
+import shutil
 
-# Source path here:
+# Source and replica paths here:
 source_dir = "./source"
+replica_dir = "./replica"
 
-# Assert that source is a directory:
-assert os.path.isdir(source_dir), f"{source_dir} is not a directory"
+# Assert that source and replica are a directory:
+assert os.path.isdir(source_dir), f"{source_dir} is not a directory."
+assert os.path.isdir(replica_dir), f"{replica_dir} is not a directory."
 
 files_source = os.listdir(source_dir)
 
 print(f"These are the files at {source_dir}:")
 for file in files_source:
+    print(file)
+    source_dir_path = os.path.join(source_dir, file)
+    replica_dir_path = os.path.join(replica_dir, file)
+    shutil.copy(source_dir_path, replica_dir_path)
+
+print(
+    f"All the files at {source_dir} have been copied to {replica_dir}.")
+
+files_replica = os.listdir(replica_dir)
+
+print(f"These are the files at {replica_dir}:")
+for file in files_replica:
     print(file)
