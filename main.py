b3c0be50cc52c5db49120ee65029cc2af4c9ab15
lpascholatti-dexter <lucas@dexterenergy.nl>
2023-09-06 13:39:17 +0200

    Commit add hash checker for files

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import os
 import shutil
 import time
 import logging
+import hashlib
+
+print("This script will synchronise a source and replica directories.")
+
+# Input source and replica paths here:
+source_dir = input("Please input the path of your source directory here: ")
+
+replica_dir = input("Please input the path of your replica directory here: ")
 
 # Configure logging:
 log_file = input(
@@ -11,10 +19,23 @@ print(f"You chose: {log_file}")
 logging.basicConfig(filename=log_file, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
-print("This script will synchronise a source and replica directories.")
+# Setup Hashlib:
+hash_algorithm = 'sha256'
+block_size = 65536
+sha256_hash = hashlib.sha256()
 
-# Input source and replica paths here:
-source_dir = input("Please input the path of your source directory here: ")
+# Function that calcualtes sha256 for the files:
+
+
+def calculate_sha256(file_path):
+    with open(file_path, "rb") as file:
+        while True:
+            data = file.read(block_size)
+            if not data:
+                break
+            sha256_hash.update(data)
+        return sha256_hash.hexdigest()
+
 
 # Create dir for source in case it does not exist:
 if not os.path.isdir(source_dir):
@@ -26,8 +47,6 @@ else:
 
 print(f"The source directory is: {source_dir}")
 logging.info(f"The source directory is: {source_dir}")
-
-replica_dir = input("Please input the path of your replica directory here: ")
 
 # Create dir for replica in case it does not exist:
 if not os.path.isdir(replica_dir):
@@ -52,6 +71,30 @@ def synchronise_directories(source_dir, replica_dir):
     print(f"These are the files at {source_dir}:\n {files_source}")
     logging.info(f"These are the files at {source_dir}:\n {files_source}")
 
+    source_hashes = []
+    replica_hashes = []
+
+    for file in files_source:
+        source_dir_path = os.path.join(source_dir, file)
+        hash_file = calculate_sha256(source_dir_path)
+        source_hashes.append(hash_file)
+
+    for file in files_replica:
+        replica_dir_path = os.path.join(replica_dir, file)
+        hash_file = calculate_sha256(replica_dir_path)
+        replica_hashes.append(hash_file)
+
+    hash_source_set = set(source_hashes)
+    print(hash_source_set)
+    hash_replica_set = set(replica_hashes)
+    print(hash_replica_set)
+
+    diff_hashes_source = hash_source_set - hash_replica_set
+    print(diff_hashes_source)
+
+    diff_hashes_replica = hash_replica_set - hash_source_set
+    print(diff_hashes_replica)
+
     # Create set from lists to get the difference in files between the two direcotires:
     source_set = set(files_source)
     replica_set = set(files_replica)
@@ -59,18 +102,18 @@ def synchronise_directories(source_dir, replica_dir):
     # Differentiate files in both directories:
     diff_source_files = source_set - replica_set
     print(
-        f"The following files are missing at {replica_dir}:\n {diff_source_files}")
+        f"The following hashes are missing at {replica_dir}:\n {diff_hashes_source}, with the following names: {diff_source_files}")
     logging.info(
-        f"The following files are missing at {replica_dir}:\n {diff_source_files}")
+        f"The following hashes are missing at {replica_dir}:\n {diff_hashes_replica}, with the following names: {diff_replica_files}")
 
     diff_replica_files = replica_set - source_set
     print(
-        f"The following files are missing at {source_dir}:\n {diff_replica_files}")
+        f"The following files are missing at {source_dir}:\n {diff_replica_files}, with the following hashes: {diff_hashes_replica}")
     logging.info(
-        f"The following files are missing at {source_dir}:\n {diff_replica_files}")
+        f"The following files are missing at {source_dir}:\n {diff_replica_files}, with the following hashes: {diff_hashes_replica}")
 
     # Copy files to replica directory
-    for file in diff_source_files:
+    for file in diff_hashes_source:
         # print(file)
         source_dir_path = os.path.join(source_dir, file)
         replica_dir_path = os.path.join(replica_dir, file)
@@ -81,7 +124,7 @@ def synchronise_directories(source_dir, replica_dir):
         f"All the different files at {source_dir} have been copied to {replica_dir}.")
 
     # Delete files missing at source
-    for file in diff_replica_files:
+    for file in diff_hashes_replica:
         replica_dir_path = os.path.join(replica_dir, file)
         os.remove(replica_dir_path)
         logging.info(f"Removed {file} from replica folder {replica_dir}")
